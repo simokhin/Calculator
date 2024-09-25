3eab65cefcf40b6191608b002e48ed89c4b684c6
simokhin <nick.simokhin@yandex.ru>
2024-09-25 14:39:14 +0300

    Make the  populate fucntion to store value

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
+// Main variables
+
 let x;
 let y;
 let operator;
+
+// Simple math functions
 
 function add(a, b){
     return a + b;
@@ -17,6 +21,8 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+// Operate function
 
 function operate(numberX, numberY, operator){
     let result;
@@ -35,48 +41,56 @@ function operate(numberX, numberY, operator){
     return result;
 }
 
-    const input = document.querySelector("#input")
-    const one = document.querySelector(".one");
-    one.addEventListener("click", (event) => {
-        populate(1);
-    })
-    const two = document.querySelector(".two");
-    two.addEventListener("click", (event) => {
-        populate(2);
-    })
-    const three = document.querySelector(".three");
-    three.addEventListener("click", (event) => {
-        populate(3);
-    })
-    const four = document.querySelector(".four");
-    four.addEventListener("click", (event) => {
-        populate(4);
-    })
-    const five = document.querySelector(".five");
-    five.addEventListener("click", (event) => {
-        populate(5);
-    })
-    const six = document.querySelector(".six");
-    six.addEventListener("click", (event) => {
-        populate(6);
-    })
-    const seven = document.querySelector(".seven");
-    seven.addEventListener("click", (event) => {
-        populate(7);
-    })
-    const eight = document.querySelector(".eight");
-    eight.addEventListener("click", (event) => {
-        populate(8);
-    })
-    const nine = document.querySelector(".nine");
-    nine.addEventListener("click", (event) => {
-        populate(9);
-    })
-    const zero = document.querySelector(".zero");
-    zero.addEventListener("click", (event) => {
-        populate(0);
-    })
+// Event listeners for numbers keys
+
+const input = document.querySelector("#input")
+const one = document.querySelector(".one");
+
+one.addEventListener("click", (event) => {
+    populate(1);
+})
+const two = document.querySelector(".two");
+two.addEventListener("click", (event) => {
+    populate(2);
+})
+const three = document.querySelector(".three");
+three.addEventListener("click", (event) => {
+    populate(3);
+})
+const four = document.querySelector(".four");
+four.addEventListener("click", (event) => {
+    populate(4);
+})
+const five = document.querySelector(".five");
+five.addEventListener("click", (event) => {
+    populate(5);
+})
+const six = document.querySelector(".six");
+six.addEventListener("click", (event) => {
+    populate(6);
+})
+const seven = document.querySelector(".seven");
+seven.addEventListener("click", (event) => {
+    populate(7);
+})
+const eight = document.querySelector(".eight");
+eight.addEventListener("click", (event) => {
+    populate(8);
+})
+const nine = document.querySelector(".nine");
+nine.addEventListener("click", (event) => {
+    populate(9);
+})
+const zero = document.querySelector(".zero");
+zero.addEventListener("click", (event) => {
+    populate(0);
+})
+
+// Function to populate and store value 
+
+let displayValue = 0;
 
 function populate(val){
     input.value += `${val}`;
+    displayValue = input.value;
 }
